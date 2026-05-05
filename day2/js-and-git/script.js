a52659e7e3409cb7a00271227a577dc3cf3c0f54
mansirathor27 <mansirathor575@gmail.com>
2026-05-05 20:09:21 +0530

    Added GitHub user lookup using fetch API

--- day2/js-and-git/script.js
+++ day2/js-and-git/script.js
@@ -9,3 +9,34 @@ toggleBtn.addEventListener("click", () => {
     toggleBtn.textContent = "🌙";
   }
 });
+
+
+async function getUser() {
+  const username = document.getElementById("username").value;
+  const resultDiv = document.getElementById("result");
+
+  if (!username) {
+    resultDiv.innerHTML = "Please enter a username";
+    return;
+  }
+
+  try {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+
+    if (data.message === "Not Found") {
+      resultDiv.innerHTML = "User not found ❌";
+      return;
+    }
+
+    resultDiv.innerHTML = `
+      <img src="${data.avatar_url}" width="100" />
+      <h3>${data.name || "No Name"}</h3>
+      <p>${data.bio || "No bio available"}</p>
+      <p>Followers: ${data.followers}</p>
+      <a href="${data.html_url}" target="_blank">View Profile</a>
+    `;
+  } catch (error) {
+    resultDiv.innerHTML = "Error fetching data";
+  }
+}
